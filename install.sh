6227e90fdc6dc1c922daa32a9ca7c1edac400a04
rozsa <rozsa.botond@protonmail.com>
2022-05-30 20:46:48 +0200

    Changes to be committed: 	new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+if (( "$EUID" == 0 )); then
+    echo "Please do not run as root"
+    exit
+fi
+
+
+
+if [[ $(lsb_release -is) == "Fedora" ]]
+then
+    echo "Installing started.."
+    if [ -d "$HOME/.config" ] 
+    then
+        echo ".config dir exist"
+    else
+        echo "making .config directory"
+        mkdir $HOME/.config 
+    fi
+
+    if [ -d "$HOME/.config/nvim" ] 
+    then
+        echo ".config/nvim dir exist"
+    else
+        echo "making .config/nvim directory"
+        mkdir $HOME/.config/nvim
+    fi
+    sudo dnf install neovim nodejs -y
+    sudo npm install -g yarn
+    pip3 install pynvim
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+else
+    echo "ERROR: Can't install on this distribution or wrong Distributor ID. (Supported: Fedora)";
+    exit
+fi
+
